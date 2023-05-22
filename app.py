3772b04bb182abebcdca5bdaa59a26ff39f81fd1
peter910820 <peter910820@gmail.com>
2023-05-22 23:16:48 +0800

    modify BaseModel

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ templates = Jinja2Templates(directory="templates")
 # DATABASE_URL =  os.getenv('DATABASE_URL')
 
 class Data(BaseModel):
-    name: str
+    username: str
+    data: str
 
 @app.get("/", response_class=HTMLResponse)
 async def product_information(request: Request):
@@ -83,14 +84,13 @@ async def show_table(request: Request):
 
 @app.post("/sd", response_class=HTMLResponse)
 async def sensor_data(request: Request, data : Data):
+    DATABASE_URL = "postgres://university_topic_user:QTv1CNqIdUAliShL1DldMYWaqV9wnhc0@dpg-cfvpfqt269v0ptn4thtg-a.oregon-postgres.render.com/university_topic"
     data_dict = data.dict()
-    print(data_dict['name'])
-    data_dict['name'] = data_dict['name'].replace('[','')
-    data_dict['name'] = data_dict['name'].replace(']','')
-    s = float(data_dict['name'][-1])
-    conn = psycopg2.connect("postgres://university_topic_user:QTv1CNqIdUAliShL1DldMYWaqV9wnhc0@dpg-cfvpfqt269v0ptn4thtg-a.oregon-postgres.render.com/university_topic", sslmode='require')
+    data = data_dict['data']
+    name = data_dict['username']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
-    cursor.execute(f"INSERT INTO sensor_data (ppm) VALUES ({s})")
+    cursor.execute(f"INSERT INTO sensor_data (username, ppm) VALUES ('{name}', {data})")
     conn.commit()
 
 if __name__ == "__main__":
--- modbus/modbusReader.py
+++ modbus/modbusReader.py
@@ -3,25 +3,27 @@ import time, requests, json
 
 DEVICE_ID = 0x1
 def modbus_read():
-    client = ModbusSerialClient( method = 'rtu', port = 'COM3',baudrate = 9600,
+    username = input('請輸入你在系統上的使用者名稱: ')
+    port = input('請輸入COM: ')
+    client = ModbusSerialClient( method = 'rtu', port = port,baudrate = 9600,
     stopbits = 1, bytesize = 8, parity = 'N', timeout = 1)
 
     if not client.connect():
-        print("!Error! Fail to connect to modbus slave")
+        print("ERROR: 'Cannot connect to modbus slave'")
         exit()
 
     while True:
-        result = client.read_holding_registers(1,8,DEVICE_ID)
+        result = client.read_holding_registers(1,1,DEVICE_ID)
+        data = int(hex(result.registers[0]), 16) *(16.04/22.4)*25*0.000001
         try:
             pr = {
-               "name": str(result.registers)
+                "username": username,
+                "data": str(data)
             }
-            print(result.registers)
+            print(data)
             requests.post("https://carbon-footprint-calculation.onrender.com/sd",json.dumps(pr))
         except:
-            print(result.function_code)
             print("error!")
         time.sleep(10)
-        # PPM*(16.04/22.4)*25*0.000001
 if __name__ == "__main__":
     modbus_read()
